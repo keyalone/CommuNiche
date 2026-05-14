a9ee216c94be70105bc038dedc6c60a68a313d38
xplmath <xplmath@zjut.edu.cn>
2026-05-14 16:39:30 +0800

    Initial commit

--- CommuNiche/__init__.py
+++ CommuNiche/__init__.py
@@ -6,5 +6,6 @@ Created on Thu May 14 10:50:21 2026
 """
 
 from .cp import *
-from utils import *
-from plot_heatmap import *
+from .utils import *
+# from .plot_heatmap import *
+# from .plot_heatmap import enrichment_heatmap
